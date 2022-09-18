b5a4cd8fe6b5bf2af8c98942da6d1dae41edea52
unknown <pankajshedage94@gmail.com>
2022-09-18 22:50:07 +0530

    Footer

---
+++ FrontEnd/src/Components/footer.js
@@ -0,0 +1,70 @@
+const Footer=()=>{
+    return(
+        <div style={{backgroundColor:"black" ,color:"white"}}>
+            
+<footer class="page-footer font-small blue pt-4">
+
+
+  <div class="container-fluid text-center text-md-left">
+
+    
+    <div class="row">
+
+      
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        
+        <h5 class="text-uppercase">E-dabba</h5>
+        <p>One cannot think well, love well, sleep well, if one has not dined well.</p>
+
+      </div>
+      
+
+      <hr className="clearfix w-100 d-md-none pb-3"/>
+
+      <div class="col-md-3 mb-md-0 mb-3">
+          </div>
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        
+        <h5 class="text-uppercase">Social Media</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            Instagram
+          </li>
+          <li>
+            Linkdin
+          </li>
+          <li>
+            Twitter
+          </li>
+          <li>
+            Facebook
+          </li>
+        </ul>
+
+      </div>
+      
+
+      
+    
+
+    </div>
+    
+
+  </div>
+  
+
+
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="/"> MDBootstrap.com</a>
+  </div>
+  
+
+</footer>
+
+        </div>
+    )
+}
+export default Footer
