63a60c8348125635e1a7613fc45cb5dcad50a8f2
vaibhav sambhaji jadhav <106860582+vaibhavjadha@users.noreply.github.com>
2022-09-23 19:52:15 +0530

    Update ProjectApplication.java
    admin hardcoded added

--- BackEnd/TiffinManagementSystem/ProjectApplication.java
+++ BackEnd/TiffinManagementSystem/ProjectApplication.java
@@ -9,4 +9,16 @@ public class ProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
+	@Bean
+    public CommandLineRunner demoData(UserDao repo ,PasswordEncoder passwordEncoder) {
+        return args -> { 
+        	User admin = repo.findByUserId(1);
+        	if(admin==null) {
+        		admin=new User(1,"admin","admin@gmail.com",null,"987456321","admin","111122223",null);
+        		admin.setPassword(passwordEncoder.encode("admin"));
+        		repo.save(admin);
+        	}
+        		
+        };
+    }
 }
